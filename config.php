0bdd0c94405e21c55e81f5201536a899ac731674
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2024-12-12 10:00:51 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "database"=> [
+        "host"=> "localhost",
+        "port"=> "3306",
+        "user"=> "root",
+        "password"=> "",
+        "dbname"=> "blog_ipb23",
+        "charset"=> "utf8mb4"
+    ]
+];
+?>
