1bae6c676ba35d84c277d7b3f6f412d8cc182cca
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-08 11:14:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-echo "<style>
-body{
-background-color: #8e809c;
-color: #c3c8e3 ;
-}
-</style>";
 
 require "functions.php";
 require "Database.php";
+
 
 //uztaisīt filtru - ierakstu meklēšana
 //1. izveido meklešanas joslu html formu, kura ir input, submit poga
@@ -37,30 +32,4 @@ if (isset($_GET["search_query"])  && $_GET["search_query"] != ""){
  $posts = $db->query($select, $params)->fetchAll();//lai nav jaraksta parak daudz citas metodes -fetchall
 
 
-echo "<h1>Blogs</h1>";
-
-echo "<form >";// post un get ir jau nokluseta 
-echo "<input name='search_query' />"; //url parametrs lai php atpazītu
-echo "<button>Meklēt</button>";//inputiem mazakas css iespējas so poga ir labaka, teksta vieta var likt svg vai bildes and all
-echo "</form>";
-
-if (count($posts) == 0 ){
-    echo "Not found. its dead 😎";
-}
-
-//3.3. dabut rezultatus
-//$posts = $statement->fetchAll(PDO::FETCH_ASSOC); // konstante 2 = PDO::FETCH_ASSOC      
-
-//dd($posts[0]["content"]);//dd($posts[0]["content"]);dd($posts[0]);
-
-//ar foreach izvada content
-echo "<ul>";
-foreach($posts as $post){
-    //echo "$post[content]<br>"; var ari šādi
-    echo"<li>" . $post["content"] . "</li>";   //kā pareizi jaraksta ul li listi br vieta
-   }
-echo "</ul>";
-
-
-
-?>
+ require "views/index.view.php";  //vienmēr ir pašā apakšā
