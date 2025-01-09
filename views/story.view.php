127cb6be76b0712555637c08418c383d61c69237
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 09:44:20 +0200

    Update story.view.php

--- views/story.view.php
+++ views/story.view.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
-    <link rel="stylesheet" href="css/style3.css">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href=" <?= $style ?>">
 </head>
 <body>
     <h1>Blogu revolūcija: populārās blogu platformas <em><?= $title ?></em> stāsts</h1>
