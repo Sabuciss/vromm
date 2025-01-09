5324f06e23a5d1f7fb19662f39fd505292536576
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 11:27:45 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,35 +2,25 @@
 
 require "functions.php";
 require "Database.php";
-
-
-//uztaisīt filtru - ierakstu meklēšana
-//1. izveido meklešanas joslu html formu, kura ir input, submit poga
-//2. kaut ka sarakstīti php
-//3. atgriest datus no sql datu bāzes
+require "router.php";
 
 $config = require("config.php");
-
-echo "Hi there  <br><br>";
-// 1. Izveido datu bāzi ar tabulu
-// ielogošanās
-//Publicēt -> savienot ar datubāzi
-// 2.Savienot php ar datu bāzi  db nosaukums , parole, lietotajvards  mysql_connect
-//3.Izvadīt datus uz html
- //  SELECT * FROM posts WHERE content LIKE \ 'slipsvitra ka to var izbeigt
 $db = new Database($config["database"]);
 
-$select = "SELECT * FROM posts"; 
-
-$params = [];//drošībai, lai db nevrētu izdzest kk random lietotājs
-if (isset($_GET["search_query"])  && $_GET["search_query"] != ""){
-    echo "Atgriest ierakstus";
-    $search_query = "%" . $_GET["search_query"] . "%";
-    $select .= " WHERE content LIKE  :nosaukums"; //sql saistītie vaicājumi un parametri   sagatavaotais vaicājums
-    $params = ["nosaukums" => $search_query];   //pats paramets                  
+if($uri == "/"){
+    require "controllers/index.php";
 }
- $posts = $db->query($select, $params)->fetchAll();//lai nav jaraksta parak daudz citas metodes -fetchall
+elseif($uri == "about"){
+    require "controllers/story.php";
+}
+elseif($uri == "categories"){
+    require "controllers/categories.php";
+}
+else{
+   http_response_code(404); 
+   echo "<p>Atvainojiet, lapa netika atrasta!</p>";
+   die();
+}
 
-$pageTitle = "Blogs";
-$style = "css/style1.css";
-require "views/index.view.php";  //vienmēr ir pašā apakšā
+
+?>
