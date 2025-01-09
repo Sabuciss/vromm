ca945300efb83e47b5fedcb002f8f90dfdbf06d4
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 10:33:25 +0200

    Add files via upload

---
+++ views/component/navbar.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Visi blogi </a></li>
+            <li><a href="/story.php">Story </a></li>
+            <li><a href="/categories.php">kategorijas </a></li>
+        </ul>
+    </nav>
+</header>
