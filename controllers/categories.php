682c12caefaca1c4887458ab811b3de4ab0adcc5
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 11:28:00 +0200

    Add files via upload

---
+++ controllers/categories.php
@@ -0,0 +1,23 @@
+<?php
+require "css/kopejais-stils.css";
+require "functions.php";
+require "Database.php";
+
+
+$config = require("config.php");
+
+$db = new Database($config["database"]);
+
+$select = "SELECT * FROM categories"; 
+
+$params = [];
+if (isset($_GET["search_query"]) && $_GET["search_query"] != ""){
+    echo "Atgriest ierakstus";
+    $search_query = "%" . $_GET["search_query"] . "%";
+    $select .= " WHERE category_name LIKE  :nosaukums"; 
+    $params = ["nosaukums" => $search_query];                 
+}
+ $categories = $db->query($select, $params)->fetchAll();
+$pageTitle ="kategorijas";
+ $style = "css/style2.css";
+ require "views/categories.view.php";
---
+++ controllers/story.php
@@ -0,0 +1,8 @@
+<?php
+require "css/kopejais-stils.css";
+$title = "Blogu revolūcija";
+
+$pageTitle = "storyy";
+$style = "css/style3.css";
+// nosūta nepieciešamo uz vajdzīgo failu
+require "views/story.view.php";
