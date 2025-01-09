289a630433c32e9fc7fa32b658fcdade95fd451a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 09:44:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -31,5 +31,5 @@ if (isset($_GET["search_query"])  && $_GET["search_query"] != ""){
 }
  $posts = $db->query($select, $params)->fetchAll();//lai nav jaraksta parak daudz citas metodes -fetchall
 
-
+$pageTitle = "Blogs";
  require "views/index.view.php";  //vienmēr ir pašā apakšā
