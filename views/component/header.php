111013b751527b5c5faeb2185662a4061e332066
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 10:55:40 +0200

    Update header.php

--- views/component/header.php
+++ views/component/header.php
@@ -3,7 +3,8 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title><? $pageTitle?></title>
-   <link rel="stylesheet" href="css/style1.css">
+   <title><?= $pageTitle ?? "😃" ?></title>
+   <link rel="stylesheet" href= "css/kopejais-stils.css">
+   <link rel="stylesheet" href= "<?php $style ?? "" ?>">
 </head>
 <body>
