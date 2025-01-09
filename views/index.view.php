7bf5e28e33e545b448b385dd34edb36bbde8ddb4
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-09 09:44:39 +0200

    Update index.view.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="lv">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Blog</title>
-   <link rel="stylesheet" href="css/style1.css">
-</head>
-<body>
+<?php require "component/header.php";?> <!-- p h p ir jabut lai stradatu-->
  <h1>Blogs</h1>
 
   <form >
