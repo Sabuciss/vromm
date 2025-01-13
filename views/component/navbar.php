03387919e8da5ba87efdc3dbdb3c8a1d4b59d973
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-13 10:51:42 +0200

    Update navbar.php

--- views/component/navbar.php
+++ views/component/navbar.php
@@ -2,8 +2,8 @@
     <nav>
         <ul>
             <li><a href="/">Visi blogi </a></li>
-            <li><a href="/story.php">Story </a></li>
-            <li><a href="/categories.php">kategorijas </a></li>
+            <li><a href="/about">Story </a></li>
+            <li><a href="/categories">kategorijas </a></li>
         </ul>
     </nav>
 </header>
