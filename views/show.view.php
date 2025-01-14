bca83ac78365c7b6637f244b542e78ee4009080a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-14 15:07:01 +0200

    Add files via upload

---
+++ views/show.view.php
@@ -0,0 +1,9 @@
+<?php require "views/component/header.php";?> 
+<?php require "views/component/navbar.php";?>
+
+ <h1><?= $post["content"]?></h1>
+
+<?php require "views/component/footer.php";?>
+
+
+
