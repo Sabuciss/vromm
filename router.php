b2e8f097c2a6cbc3ce8d97d7450740beb6e9dc8f
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-14 13:34:07 +0200

    Update router.php

--- router.php
+++ router.php
@@ -1,21 +1,15 @@
 <?php 
-require "functions.php";
 
 
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
-if($uri == "/"){
-    require "controllers/index.php";
-}
-elseif($uri == "/about"){
-    require "controllers/story.php";
-}
-elseif($uri == "/categories"){
-    require "controllers/categories.php";
-}
-else{
+$routes = require("routes.php");
+
+if(array_key_exists($uri, $routes)){ //uzreiz parbauda vai eksiste un ja ja tad aizsuta uz to ko mekle, nevsi iet pa vienam un parbauda bet uzreiz visu
+    require $routes[$uri];
+}else{
    http_response_code(404); 
-   echo "<p>Atvainojiet, lapa netika atrasta!</p>";
+   require "controllers/404.php";
    die();
 }
 
