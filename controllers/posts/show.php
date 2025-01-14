5be3a983aa27fc49d173e25d579f1e278f53425d
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-14 15:11:26 +0200

    Update show.php

--- controllers/posts/show.php
+++ controllers/posts/show.php
@@ -4,7 +4,7 @@ if(!isset($_GET["id"]) || $_GET["id"] == ""){
  redirectIFNotFound();
     
 }
-   $sql = "SELECT * FROM posts WHERE id == :id";
+   $sql = "SELECT * FROM posts WHERE id = :id";
     $params = ["id" => $_GET["id"]];
     $post = $db->query($sql, $params)->fetch();
 
