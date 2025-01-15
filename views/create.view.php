6c700a094b5f8f3a3b27da014414f1a83cb78cc4
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-15 10:33:00 +0200

    Update create.view.php

--- views/create.view.php
+++ views/create.view.php
@@ -3,17 +3,10 @@
 
 <h1>Izveido bloga ierakstu</h1>
 
-<form method="POST" action="/store">
+<form method="POST" >
     <label>
-        Nosaukums:
-        <input type="text" name="title" value="<?= isset($title) ? htmlspecialchars($title) : '' ?>" required />
+        <textarea name="content"  required><?= isset($content) ? htmlspecialchars($content) : '' ?></textarea>
     </label>
-    <br />
-
-    <label>
-        <textarea name="content" rows="5" required><?= isset($content) ? htmlspecialchars($content) : '' ?></textarea>
-    </label>
-    <br />
     <button type="submit">Izveidot</button>
 </form>
 
