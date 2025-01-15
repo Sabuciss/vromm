b583b4618caacd6a117dda308fbf6839376362fe
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-15 10:52:21 +0200

    Update show.view.php

--- views/show.view.php
+++ views/show.view.php
@@ -1,7 +1,7 @@
 <?php require "views/component/header.php";?> 
 <?php require "views/component/navbar.php";?>
 
- <h1><?= $post["content"]?></h1>
+ <h1><?= htmlspecialchars($post["content"])?></h1>
 
 <?php require "views/component/footer.php";?>
 
