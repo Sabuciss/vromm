8e7b983554183895324708aa97e7e9c767d97f7a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-16 10:48:36 +0200

    Update create.view.php

--- views/create.view.php
+++ views/create.view.php
@@ -2,15 +2,15 @@
 <?php require "views/component/navbar.php";?>
 
 <h1>Izveido bloga ierakstu</h1>
-
-<form method="POST">
-    <label>
-        <input name="content" value="<?=htmlspecialchars($_POST['content'] ?? '')?> " ></input><button class="button"type="submit">Izveidot</button>
-    </label>
-    <?php if (isset($errors["content"])): ?>
-        <p class="error"><?= htmlspecialchars($errors["content"]) ?></p>
-    <?php endif; ?>
-    
-</form>
-
+<div class="container">
+    <form method="POST">
+        <label>
+            <input name="content" value="<?=htmlspecialchars($_POST['content'] ?? '')?> " ></input><button class="button"type="submit">Izveidot</button>
+        </label>
+        <?php if (isset($errors["content"])): ?>
+            <p class="error"><?= htmlspecialchars($errors["content"]) ?></p>
+        <?php endif; ?>
+        
+    </form>
+</div?
 <?php require "views/component/footer.php";?>
