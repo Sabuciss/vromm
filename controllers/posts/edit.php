4c8b691324d4de1a87b3860d6db7b6c82a6656a6
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-16 11:27:42 +0200

    Update edit.php

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -2,23 +2,11 @@
 <?php
 
 require "Validator.php";
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $errors = [];
-    
-    if (!Validator::string($_POST["content"], max: 50)){ //kad nepieciešams ilgtermiņā piešķirt kk īpašību, kopīgota starp vairākām metodēm
-        $errors["content"] = "Saturam ir jabut, bet ne garākam par 50 rakstzīmēm";
-    }
    
-    elseif (empty($errors)) { //ja nav kļudu tad ievada db
-        $sql = "INSERT INTO posts (content) VALUES (:content)";
-        $params = ["content" => $_POST["content"]];
-        $db->query($sql, $params);
+$sql = "SELECT * FROM posts WHERE id = :id";
+$params = ["id" => $_GET["id"]];
+$post = $db->query($sql, $params)->fetch();
 
-        header("Location: /");
-        exit();
-    }
-}
 
 $pageTitle = "Edits";
 $style = "css/kopejais-stils.css";
