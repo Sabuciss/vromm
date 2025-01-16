548481223a85c472e46249e92538b7c9f5d899f6
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-16 11:26:59 +0200

    Update navbar.php

--- views/component/navbar.php
+++ views/component/navbar.php
@@ -5,6 +5,7 @@
             <li><a href="/about">Story </a></li>
             <li><a href="/categories">kategorijas </a></li>
             <li><a href="/create">createe </a></li>
+            <li><a href="/edit">editots </a></li>
         </ul>
     </nav>
 </header>
