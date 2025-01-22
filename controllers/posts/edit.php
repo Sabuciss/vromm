72530bac4ef42d696d8b070c2098faecbca436f3
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-22 16:00:00 +0200

    Update edit.php

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -1,12 +1,20 @@
-
 <?php
 
 require "Validator.php";
-   
+
+if (!isset($_GET["id"]) || empty($_GET["id"])) {
+    die("Ieraksta ID nav padots!"); 
+}
+$id = intval($_GET["id"]);
+
 $sql = "SELECT * FROM posts WHERE id = :id";
-$params = ["id" => $_GET["id"]];
+$params = ["id" => $id];
+
 $post = $db->query($sql, $params)->fetch();
 
+if (!$post) {
+    die("Ieraksts netika atrasts!"); 
+}
 
 $pageTitle = "Edits";
 $style = "css/kopejais-stils.css";
