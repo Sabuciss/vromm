ddfa4cb133fa2797dc9102e9ee2c9eff04eaddca
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-22 18:53:40 +0200

    Update edit.view.php

--- views/edit.view.php
+++ views/edit.view.php
@@ -2,23 +2,18 @@
 <?php require "views/component/navbar.php"; ?>
 
 <h1>Rediģēt ierakstu</h1>
+
 <div class="container">
-    <form method="POST" action="update.php">
-        <input type="hidden" name="id" value="<?= htmlspecialchars($post['id']) ?>">
-        
-        <div>
-            <label for="title">Virsraksts:</label>
-            <input type="text" id="title" name="title" value="<?= htmlspecialchars($post['title']) ?>" required>
-        </div>
-        
-        <div>
-            <label for="content">Saturs:</label>
-            <textarea id="content" name="content" rows="10" required><?= htmlspecialchars($post['content']) ?></textarea>
-        </div>
-        
-        <button type="submit">Saglabāt izmaiņas</button>
+    <form method="POST" action="/edit.php">
+        <input type="hidden" name="id" value="<?= htmlspecialchars($post["id"]) ?>">
+
+        <label for="content">Saturs</label>
+        <input 
+            type="text" id="content"  name="content" value="<?= htmlspecialchars($post["content"] ?? '') ?>" 
+        >
+
+        <button type="submit">Saglabāt</button>
     </form>
 </div>
 
 <?php require "views/component/footer.php"; ?>
-
