b3a9d19fb2cb089124d99e637269f7f429d77443
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-22 18:38:27 +0200

    Update show.view.php

--- views/show.view.php
+++ views/show.view.php
@@ -2,6 +2,8 @@
 <?php require "views/component/navbar.php";?>
 
  <h1><?= htmlspecialchars($post["content"])?></h1>
+<a href="edit?id=<?= htmlspecialchars($post["id"]) ?>">Rediģēt </a>
+
 
 <?php require "views/component/footer.php";?>
 
