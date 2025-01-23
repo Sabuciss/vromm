b513159f9f73c6c3c28b6c1068a696ce55416ee3
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-23 15:10:37 +0200

    Update show.view.php

--- views/show.view.php
+++ views/show.view.php
@@ -3,9 +3,11 @@
 
 <h1><?= htmlspecialchars($post["content"])?></h1>
 
-<a href="edit?id=<?=$post["id"] ?>">Rediģēt </a>
-<!-- <form method="POST" action="/delete.php" onsubmit="return confirm('Vai tiešām vēlaties dzēst šo ierakstu?');">
-    <input type="hidden" name="id" value="<?= htmlspecialchars($post['id']) ?>">
-    <button type="submit">Dzēst</button>
-</form> -->
+<button><a href="edit?id=<?=$post["id"] ?>">Rediģēt </a><button>
+ 
+ <form method="POST" action="/delete" >
+ <input name="id" value="<?= $post["id"]  ?>" type="hidden" />
+    <input type="submit" value="dzest"/>
+</form> 
+
 <?php require "views/component/footer.php";?>
