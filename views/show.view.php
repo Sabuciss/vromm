ae401c91c426bd2df1e44610261eaf434100678b
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-23 14:40:36 +0200

    Update show.view.php

--- views/show.view.php
+++ views/show.view.php
@@ -1,15 +1,11 @@
 <?php require "views/component/header.php";?> 
 <?php require "views/component/navbar.php";?>
 
- <h1><?= htmlspecialchars($post["content"])?></h1>
+<h1><?= htmlspecialchars($post["content"])?></h1>
 
-<a href="edit?id=<?= htmlspecialchars($post["id"]) ?>">Rediģēt </a>
+<a href="edit?id=<?=$post["id"] ?>">Rediģēt </a>
 <!-- <form method="POST" action="/delete.php" onsubmit="return confirm('Vai tiešām vēlaties dzēst šo ierakstu?');">
     <input type="hidden" name="id" value="<?= htmlspecialchars($post['id']) ?>">
     <button type="submit">Dzēst</button>
 </form> -->
-
 <?php require "views/component/footer.php";?>
-
-
-
