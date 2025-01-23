65d45ab62342777064d45e5c6a173d3d6b680aea
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-23 14:39:53 +0200

    Update edit.php

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -1,39 +1,31 @@
+
 <?php
 
 require "Validator.php";
-require "Database.php";
 
-$config = require("config.php");
-$db = new Database($config["database"]);
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (!Validator::string($_POST["content"], max: 50)) {
-        die("Saturs nav derīgs.");
-    }
-
-    if (!is_numeric($_POST["id"])) {
-        die("ID nav derīgs.");
-    }
-
-    $sqlCheck = "SELECT * FROM posts WHERE id = :id";
-    $post = $db->query($sqlCheck, ["id" => $_POST["id"]])->fetch();
-    if (!$post) {
-        die("Ieraksts ar norādīto ID neeksistē.");
-    }
-
-    $sqlUpdate = "UPDATE posts SET content = :content WHERE id = :id";
-    $db->query($sqlUpdate, [
-        "content" => $_POST["content"],
-        "id" => $_POST["id"]
-    ]);
-
-    header("Location: /show?id=" . $_POST["id"]);
-    exit();
+if(isset($_GET["id"])){
+    $sql = "SELECT * FROM posts WHERE id = :id;"; 
+    $params = ["id" => $_GET["id"]];
+    $post = $db->query($sql, $params)->fetch();
 }
 
-header("Location: /");
-exit();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $errors = [];
+    
+    if (!Validator::string($_POST["content"], max: 50)){
+    $errors["content"]= "Saturam ir jabut";
+    }
+   
+    elseif (empty($errors)) {
+        $sql = "UPDATE posts SET content = :content WHERE id = :id;";
+        $params = ["id" => $_POST["id"], "content" => $_POST["content"] ];
+        $post = $db->query($sql, $params)->fetch();
+
+        header("Location: /show?id=" . $_POST["id"]);
+        exit();
+    }
+}
 
  $pageTitle = "edits";
- require "views/edit.view.php";
-
+ $style = "css/kopejais-stils.css";
+ require "views/posts/edit.view.php";
