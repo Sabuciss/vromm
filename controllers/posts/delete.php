0e5b96ee62a71d2eb023244ce74c8e7c2df7c4de
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-23 17:40:20 +0200

    Update delete.php

--- controllers/posts/delete.php
+++ controllers/posts/delete.php
@@ -1,14 +1,13 @@
 <?php
 
-        $sql = "DELETE into posts  WHERE id = :id;";
-        
-        $params = ["id" => $_POST["id"]];
-        
-        $post = $db->query($sql, $params)->fetch();
+$sql = "DELETE FROM posts WHERE id = :id;"; 
 
-        header("Location: /");
-        exit();
+$params = ["id" => $_POST["id"]]; // Ensure id is coming from a valid POST request
 
+$post = $db->query($sql, $params); // No need for fetch() since DELETE doesn't return rows
+
+header("Location: /");
+exit();
  $pageTitle = "edits";
  $style = "css/kopejais-stils.css";
  require "views/posts/edit.view.php";
