73ecffa9fb8e97837e9229d2578b49628289aa5f
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-12 13:32:26 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -7,6 +7,16 @@ return [
         "/categories"=> "controllers/categories.php",
         "/create"=> "controllers/posts/create.php",
         "/edit"=> "controllers/posts/edit.php",
-        "/delete"=> "controllers/posts/delete.php"
+        "/delete"=> "controllers/posts/delete.php",
+
+
+        
+        "/categories/create"=> "controllers/categories/posts/create.php",
+        "/categories/index"=> "controllers/posts/create/inde.php",
+        "/categories/show"=> "controllers/posts/edit/show.php",
+        "/categories/edit"=> "controllers/posts/delete/edit.php",
+        "/categories/delete"=> "controllers/posts/delete.php"
+
+        
 ];
 ?>
