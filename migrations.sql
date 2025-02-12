54d2baa165de927af814bee057e57a8362bae2a2
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-12 13:39:55 +0200

    Update migrations.sql

--- migrations.sql
+++ migrations.sql
@@ -6,7 +6,8 @@ USE blog_ipb23;
 
 CREATE TABLE posts (
     id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
-    content VARCHAR (5200)
+    content VARCHAR (5200),
+    category_id INT
 );
 /*ielikt saturu post tabula*/
 INSERT INTO posts
