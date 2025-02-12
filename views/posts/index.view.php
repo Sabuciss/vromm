59f696b3dccb0b6e7b54c58a1fddc28a59db3a8a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-12 22:14:48 +0200

    Add files via upload

---
+++ views/posts/index.view.php
@@ -0,0 +1,41 @@
+<?php require "views/component/header.php";?> <!-- p h p ir jabut lai stradatu-->
+<?php require "views/component/navbar.php";?>
+ <h1>Blogs</h1>
+
+<div class="container">
+   <form >
+      <input name='search_query' value='<?= $_GET["search_query"] ?? ""?>'  />
+      <button>Meklēt</button>
+   </form>
+
+   <?php if (count($posts) == 0 ){ ?>
+      <p>Not found. its dead 😎</p>
+   <?php } ?>
+
+   <ul>
+      <?php foreach($posts as $post){ ?>
+      <li><a href="show?id=<?= $post["id"]?>"> <?= htmlspecialchars($post["content"])?></a></li> <!-- php echo-->
+      <?php   } ?>
+   </ul>
+</div>
+  <!--
+     1. if else
+     if(isset($x)){
+        $y =$x;
+     }
+     else{
+        $y = "nav vertibas";
+     }
+
+     2. ternary operator trīškārsais operators
+     $y = isset($x) ? $x : "nav vertibas";
+
+     3. null coalescing operator
+     $y = $x ?? "nav vertibas";  simtactic sugar
+
+
+  -->
+  <?php require "views/component/footer.php";?>
+
+
+
---
+++ views/posts/show.view.php
@@ -0,0 +1,15 @@
+<?php require "views/component/header.php";?> 
+<?php require "views/component/navbar.php";?>
+
+<h1><?= htmlspecialchars($post["content"])?></h1>
+
+<p><strong>Kategorija:</strong> <?= $post['category_name'] ?: "Nav kategorijas" ?></p>
+
+<button><a href="edit?id=<?=$post["id"] ?>">Rediģēt </a><button>
+ 
+ <form method="POST" action="/delete" >
+ <input name="id" value="<?= $post["id"]  ?>" type="hidden" />
+    <input type="submit" value="dzest"/>
+</form> 
+
+<?php require "views/component/footer.php";?>
