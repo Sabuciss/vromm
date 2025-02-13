a272e2663d59e0797fe92d6130b68b9f5a576578
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-13 21:21:17 +0200

    kategorijas update

--- views/posts/edit.view.php
+++ views/posts/edit.view.php
@@ -1,25 +1,31 @@
 <?php require "views/component/header.php"; ?>
 <?php require "views/component/navbar.php"; ?>
 
+<h1>Rediģēt bloga ierakstu</h1>
 <div class="container">
+    <form method="POST">
+        
+        <label for="category_id">Kategorija:</label>
+        <select id="category_id" name="category_id">
+            <?php foreach ($categories as $category): ?>
+                <option value="<?= $category['id'] ?>" 
+                    <?= ($post['category_id'] == $category['id']) ? 'selected' : '' ?>>
+                    <?= $category['category_name'] ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <?php if (isset($errors["category_id"])): ?>
+            <p class="error"><?= $errors["category_id"] ?></p>
+        <?php endif; ?>
 
-    <h1>Redige ieraksu</h1>
+        <label for="content">Saturs:</label>
+        <input type="text" id="content" name="content" value="<?= $_POST['content'] ?? $post['content'] ?>" />
+        <?php if (isset($errors["content"])): ?>
+            <p class="error"><?= $errors["content"] ?></p>
+        <?php endif; ?>
 
-        <form method="POST">
-
-        <label>
-        <input type="hidden" name="id" value="<?= $post["id"] ?>" />
-        </label>
-
-    
-        <label>
-        <input type="text" name="content" value="<?= $post["content"] ?? '' ?>" id="ieraksts" >
-        <button type="submit">Saglabāt</button> 
-        </label>
-
+        <button class="button" type="submit">Saglabāt izmaiņas</button>
     </form>
-
-
 </div>
 
 <?php require "views/component/footer.php"; ?>
