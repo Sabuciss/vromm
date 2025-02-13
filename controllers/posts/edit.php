7bb3ea2c42743008c84b4eb1cde7e8a098218375
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-13 21:24:35 +0200

    kategorijas update

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -1,23 +1,3 @@
-
-<?php
-
-require "Validator.php";
-
-if(isset($_GET["id"])){
-    $sql = "SELECT * FROM posts WHERE id = :id;"; 
-    $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $errors = [];
-    
-    if (!Validator::string($_POST["content"], max: 50)){
-    $errors["content"]= "Saturam ir jabut";
-    }
-   
-    elseif (empty($errors)) {
-        $sql = "UPDATE posts SET content = :content WHERE id = :id;";
 <?php
 
 require "Validator.php";
@@ -55,13 +35,3 @@ $categories = $db->query($sql, []);
 $pageTitle = "Edit";
 $style = "css/kopejais-stils.css";
 require "views/posts/edit.view.php";
-        $post = $db->query($sql, $params)->fetch();
-
-        header("Location: /show?id=" . $_POST["id"]);
-        exit();
-    }
-}
-
- $pageTitle = "edits";
- $style = "css/kopejais-stils.css";
- require "views/posts/edit.view.php";
