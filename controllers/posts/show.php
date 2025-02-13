2f460781b42393d7b3d6f35133c0abb321b079b7
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-13 19:17:32 +0200

    kategorijas update

--- controllers/posts/show.php
+++ controllers/posts/show.php
@@ -1,14 +1,20 @@
 <?php
 
-if(!isset($_GET["id"]) || $_GET["id"] == ""){
- redirectIFNotFound();
-    
-}
-   $sql = "SELECT * FROM posts WHERE id = :id";
-    $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
-
-if(!$post){
+if (!isset($_GET["id"]) || $_GET["id"] == "") {
     redirectIFNotFound();
 }
+
+$sql = "SELECT posts.*, categories.category_name 
+        FROM posts
+        LEFT JOIN categories 
+        ON posts.category_id = categories.id 
+        WHERE posts.id = :id";
+
+$params = ["id" => $_GET["id"]];
+$post = $db->query($sql, $params)->fetch();
+
+if (!$post) {
+    redirectIFNotFound();
+}
+
 require "views/posts/show.view.php";
