099a03fd15d89b984ad42edf5dacb81205593727
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-14 11:45:04 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -11,11 +11,11 @@ return [
 
 
         
-     "/categories/create" => "controllers/categories/create.php",
-        "/categories/index"  => "controllers/categories/index.php",
-        "/categories/show"   => "controllers/categories/show.php",
-        "/categories/edit"   => "controllers/categories/edit.php",
-        "/categories/delete" => "controllers/categories/delete.php"
+        "/categories/create"=> "controllers/categories/create.php",
+        "/categories/index"=> "controllers/categories/create/index.php",
+        "/categories/show"=> "controllers/categories/show.php",
+        "/categories/edit"=> "controllers/categories/edit.php",
+        "/categories/delete"=> "controllers/categories/delete.php"
 
 
         
