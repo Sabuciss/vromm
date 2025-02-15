773796ec6fe5653f07ecd7964cd5ba42df96b80b
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-15 22:31:32 +0200

    kategorijas

--- controllers/categories/show.php
+++ controllers/categories/show.php
@@ -4,11 +4,16 @@ if(!isset($_GET["id"]) || $_GET["id"] == ""){
  redirectIFNotFound();
     
 }
-   $sql = "SELECT * FROM posts WHERE id = :id";
+   $sql = "SELECT posts.* , categories.category_name FROM posts
+           LEFT JOIN categories
+           ON posts.category_id = categories.id
+           WHERE posts.id = :id ;";
     $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
+    $category = $db->query($sql, $params)->fetch();
 
-if(!$post){
+if(!$category){
     redirectIFNotFound();
 }
+$pageTitle ="kategorijas";
+$style = "css/kopejais-stils.css";
 require "views/posts/show.view.php";
