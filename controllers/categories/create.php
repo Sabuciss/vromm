73ea014a488665b6a66a59268ed659c68ff6bc79
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-15 22:27:30 +0200

    kategorijas up

--- controllers/categories/create.php
+++ controllers/categories/create.php
@@ -2,27 +2,16 @@
 
 require "Validator.php";
 
-$sql = "SELECT * FROM categories";
-$categories = $db->query($sql)->fetchAll();
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = [];
     
-    if (!Validator::string($_POST["content"], min: 3, max: 50)) {
-        $errors["content"] = "Saturam ir jābūt, bet ne garākam par 50 rakstzīmēm.";
+    if (!Validator::string($_POST["content"], min:3, max: 50)){ 
+        $errors["content"] = "Saturam ir jabut, bet ne garākam par 50 rakstzīmēm";
     }
-
-    // Validācija kategorijai ja ir izvēlēta derīga kategorija
-    if (empty($_POST["category_id"]) || !is_numeric($_POST["category_id"])) {
-        $errors["category_id"] = "Lūdzu, izvēlies derīgu kategoriju.";
-    }
-
-    if (empty($errors)) { // Ja nav kļūdu, saglabā datus datu bāzē
-        $sql = "INSERT INTO posts (content, category_id) VALUES (:content, :category_id)";
-        $params = [
-            "content" => $_POST["content"],
-            "category_id" => $_POST["category_id"]
-        ];
+   
+    elseif (empty($errors)) {
+        $sql = "INSERT INTO posts (content) VALUES (:content)";
+        $params = ["content" => $_POST["content"]];
         $db->query($sql, $params);
 
         header("Location: /");
@@ -30,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-$pageTitle = "Pievienot ierakstu";
+$pageTitle = "ierakstss";
 $style = "css/kopejais-stils.css";
 
-require "views/posts/create.view.php";
+require "views/categories/create.view.php";
