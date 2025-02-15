8cbfe980e89c3d9335b5abda46c82169b9228d2b
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-15 22:33:05 +0200

    kategorijas

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -1,3 +1,4 @@
+
 <?php
 
 require "Validator.php";
@@ -12,26 +13,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = [];
     
     if (!Validator::string($_POST["content"], max: 50)){
-        $errors["content"]= "Saturam ir jābūt";
+    $errors["content"]= "Saturam ir jabut";
     }
    
     elseif (empty($errors)) {
-        $sql = "UPDATE posts SET content = :content, category_id = :category_id WHERE id = :id;";
-        $params = [
-            "id" => $_POST["id"], 
-            "content" => $_POST["content"],
-            "category_id" => $_POST["category_id"]
-        ];
-        $db->query($sql, $params);
+        $sql = "UPDATE posts SET content = :content WHERE id = :id;";
+        $params = ["id" => $_POST["id"], "content" => $_POST["content"] ];
+        $post = $db->query($sql, $params)->fetch();
 
         header("Location: /show?id=" . $_POST["id"]);
         exit();
     }
 }
 
-$sql = "SELECT * FROM categories";
-$categories = $db->query($sql, []); 
-
-$pageTitle = "Edit";
-$style = "css/kopejais-stils.css";
-require "views/posts/edit.view.php";
+ $pageTitle = "edits";
+ $style = "css/kopejais-stils.css";
+ require "views/posts/edit.view.php";
