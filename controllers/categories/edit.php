961567ceb1e1035b76f0fa4d905d2d6b6db06e9a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-15 22:29:53 +0200

    kategorijas

--- controllers/categories/edit.php
+++ controllers/categories/edit.php
@@ -4,28 +4,28 @@
 require "Validator.php";
 
 if(isset($_GET["id"])){
-    $sql = "SELECT * FROM posts WHERE id = :id;"; 
+    $sql = "SELECT * FROM categories WHERE id = :id;"; 
     $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
+    $category = $db->query($sql, $params)->fetch();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = [];
     
-    if (!Validator::string($_POST["content"], max: 50)){
-    $errors["content"]= "Saturam ir jabut";
+    if (!Validator::string($_POST["category_name"], max: 50)){
+    $errors["category_name"]= "Saturam ir jabut";
     }
    
     elseif (empty($errors)) {
-        $sql = "UPDATE posts SET content = :content WHERE id = :id;";
-        $params = ["id" => $_POST["id"], "content" => $_POST["content"] ];
-        $post = $db->query($sql, $params)->fetch();
+        $sql = "UPDATE categories SET category_name = :category_name WHERE id = :id;";
+        $params = ["id" => $_POST["id"], "category_name" => $_POST["category_name"] ];
+        $category = $db->query($sql, $params)->fetch();
 
-        header("Location: /show?id=" . $_POST["id"]);
+        header("Location: /categories/show?id=" . $_POST["id"]);
         exit();
     }
 }
 
- $pageTitle = "edits";
+ $pageTitle = "Kategorijas edits";
  $style = "css/kopejais-stils.css";
- require "views/posts/edit.view.php";
+ require "views/categories/edit.view.php";
