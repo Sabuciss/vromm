8ebee3638c67055b36d879330b7e7202168af161
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:05:16 +0200

    kategorijass

--- views/categories/edit.view.php
+++ views/categories/edit.view.php
@@ -5,22 +5,26 @@
 <div class="container">
     <form method="POST">
         
+        <input type="hidden" name="id" value="<?= $category['id'] ?>" />
+
         <label for="category_id">Kategorija:</label>
         <select id="category_id" name="category_id">
         
-            <?php foreach ($categories as $category): ?>
-                <option value="<?= $category['id'] ?>" 
-                    <?= ($post['category_id'] == $category['id']) ? 'selected' : '' ?>>
-                    <?= $category['category_name'] ?>
+            <?php foreach ($categories as $category_option): ?>
+                <option value="<?= $category_option['id'] ?>" 
+                    <?= ($category['id'] == $category_option['id']) ? 'selected' : '' ?>>
+                    <?= $category_option['category_name'] ?>
                 </option>
             <?php endforeach; ?>
         </select>
+        
         <?php if (isset($errors["category_id"])): ?>
             <p class="error"><?= $errors["category_id"] ?></p>
         <?php endif; ?>
 
         <label for="content">Saturs:</label>
-        <input type="text" id="content" name="content" value="<?= $_POST['content'] ?? $post['content'] ?>" />
+        <input type="text" id="content" name="content" value="<?= $_POST['content'] ?? $category['category_name'] ?>" />
+        
         <?php if (isset($errors["content"])): ?>
             <p class="error"><?= $errors["content"] ?></p>
         <?php endif; ?>
