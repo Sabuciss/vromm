e91a4ae627599369ce9415bf0c4860c6e449af19
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:12:05 +0200

    kategorijass

--- controllers/categories/create.php
+++ controllers/categories/create.php
@@ -1,15 +1,17 @@
 <?php
 
 require "Validator.php";
+require "Database.php";
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = [];
     
-    if (!Validator::string($_POST["content"], min:3, max: 50)){ 
+    if (!Validator::string($_POST["content"], min:3, max: 50)){ //kad nepieciešams ilgtermiņā piešķirt kk īpašību, kopīgota starp vairākām metodēm
         $errors["content"] = "Saturam ir jabut, bet ne garākam par 50 rakstzīmēm";
     }
    
-    elseif (empty($errors)) {
+    elseif (empty($errors)) { //ja nav kļudu tad ievada db
         $sql = "INSERT INTO posts (content) VALUES (:content)";
         $params = ["content" => $_POST["content"]];
         $db->query($sql, $params);
