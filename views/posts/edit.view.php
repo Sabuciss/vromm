dea7e00877c362e4a58fc196bde8693002328f72
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:03:41 +0200

    kategorijas

--- views/posts/edit.view.php
+++ views/posts/edit.view.php
@@ -2,26 +2,31 @@
 <?php require "views/component/navbar.php"; ?>
 
 <h1>Rediģēt bloga ierakstu</h1>
+
 <div class="container">
     <form method="POST">
-        
+        <input type="hidden" name="id" value="<?= htmlspecialchars($post['id']) ?>" />
+
         <label for="category_id">Kategorija:</label>
-        <select id="category_id" name="category_id">
+        <select name="category_id" id="category_id">
+            <option value="">Izvēlies kategoriju</option>
             <?php foreach ($categories as $category): ?>
-                <option value="<?= $category['id'] ?>" 
+                <option value="<?= htmlspecialchars($category['id']) ?>" 
                     <?= ($post['category_id'] == $category['id']) ? 'selected' : '' ?>>
-                    <?= $category['category_name'] ?>
+                    <?= htmlspecialchars($category['category_name']) ?>
                 </option>
             <?php endforeach; ?>
         </select>
+
         <?php if (isset($errors["category_id"])): ?>
-            <p class="error"><?= $errors["category_id"] ?></p>
+            <p class="error"><?= htmlspecialchars($errors["category_id"]) ?></p>
         <?php endif; ?>
 
         <label for="content">Saturs:</label>
-        <input type="text" id="content" name="content" value="<?= $_POST['content'] ?? $post['content'] ?>" />
+        <input type="text" id="content" name="content" value="<?= htmlspecialchars($_POST['content'] ?? $post['content']) ?>" />
+        
         <?php if (isset($errors["content"])): ?>
-            <p class="error"><?= $errors["content"] ?></p>
+            <p class="error"><?= htmlspecialchars($errors["content"]) ?></p>
         <?php endif; ?>
 
         <button class="button" type="submit">Saglabāt izmaiņas</button>
