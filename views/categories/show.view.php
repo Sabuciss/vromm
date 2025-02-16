527cb284d341d3636203341ea02aedb65ac65228
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:04:32 +0200

    kategorijass

--- views/categories/show.view.php
+++ views/categories/show.view.php
@@ -1,12 +1,14 @@
 <?php require "views/component/header.php";?> 
 <?php require "views/component/navbar.php";?>
 
-<h1><?= htmlspecialchars($category["category_name"])?></h1>
+<h1><?= htmlspecialchars($post["content"])?></h1>
 
-<button><a href="edit?id=<?=$category["id"] ?>">Rediģēt </a><button>
+<p><strong>Kategorija:</strong> <?= $post['category_name'] ?? "Nav kategorijas" ?></p><!--Null pārbaude (??) – Ja category_name nav, parādīs "Nav kategorijas".-->
+
+<button class="button"><a href="edit?id=<?=$post["id"] ?>">Rediģēt </a><button>
  
  <form method="POST" action="/delete" >
- <input name="id" value="<?= $category["category_id"]  ?>" type="hidden" />
+ <input name="id" value="<?= $post["id"]  ?>" type="hidden" />
     <input type="submit" value="dzest"/>
 </form> 
 
