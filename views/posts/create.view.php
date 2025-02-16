72259eb704a7af7c41c76393cc3d22ac7bdc85c3
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:04:04 +0200

    kategorijass

--- views/posts/create.view.php
+++ views/posts/create.view.php
@@ -1,35 +1,33 @@
-<?php require "views/component/header.php"; ?> 
+<?php require "views/component/header.php"; ?>
 <?php require "views/component/navbar.php"; ?>
 
-<h1>Izveido bloga ierakstu</h1>
+<h1>Izveidot jaunu bloga ierakstu</h1>
+
 <div class="container">
     <form method="POST">
-       
-        <div>
-            <label for="category_id">Kategorija:</label>
-            <select id="category_id" name="category_id">
-                <option value=""> kategorija</option>
-                <?php foreach ($categories as $category): ?>
-                    <option value="<?= $category['id'] ?>" 
-                        <?= (isset($_POST['category_id']) && $_POST['category_id'] == $category['id']) ? 'selected' : '' ?>>
-                        <?= $category['category_name'] ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-            <?php if (isset($errors["category_id"])): ?>
-                <p class="error"><?= $errors["category_id"] ?></p>
-            <?php endif; ?>
-        </div>
+        <label for="category_id">Kategorija:</label>
+        <select name="category_id" id="category_id">
+            <option value="">Izvēlies </option>
+            <?php foreach ($categories as $category): ?>
+                <option value="<?= htmlspecialchars($category['id']) ?>" 
+                    <?= (isset($_POST["category_id"]) && $_POST["category_id"] == $category['id']) ? "selected" : "" ?>>
+                    <?= htmlspecialchars($category['category_name']) ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
 
-        <div>
-            <label for="content">Saturs:</label>
-            <textarea id="content" name="content"><?= $_POST['content'] ?? '' ?></textarea>
-            <?php if (isset($errors["content"])): ?>
-                <p class="error"><?= $errors["content"] ?></p>
-            <?php endif; ?>
-        </div>
+        <?php if (isset($errors["category_id"])): ?>
+            <p class="error"><?= $errors["category_id"] ?></p>
+        <?php endif; ?>
 
-        <button class="button" type="submit">Izveidot</button>
+        <label for="content">Saturs:</label>
+        <input type="text" name="content" value="<?= htmlspecialchars($_POST['content'] ?? '') ?>" />
+
+        <?php if (isset($errors["content"])): ?>
+            <p class="error"><?= $errors["content"] ?></p>
+        <?php endif; ?>
+
+        <button class="button" type="submit">Izveido</button>
     </form>
 </div>
 
