a941fb270d481f8fc78a28d9e8cde3cdddb148c2
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-16 16:05:45 +0200

    kategorijass

--- views/categories/create.view.php
+++ views/categories/create.view.php
@@ -1,39 +1,19 @@
-<?php require "views/component/header.php"; ?> 
+<?php require "views/component/header.php"; ?>
 <?php require "views/component/navbar.php"; ?>
 
-<h1>Izveido bloga ierakstu</h1>
+<h1>Izveido kategoriju</h1>
 <div class="container">
     <form method="POST">
-       
-        <div>
-            <label for="category_id">Kategorija:</label>
-            <select id="category_id" name="category_id">
-                <option value=""> ...</option>
-                <option value=""> Freestyle</option>
-                <option value="sports"> Sports</option>
-                <option value=""> Svētki</option>
-                <option value=""> Mūzika</option>
-                <?php foreach ($categories as $category): ?>
-                    <option value="<?= $category['id'] ?>" 
-                        <?= (isset($_POST['category_id']) && $_POST['category_id'] == $category['id']) ? 'selected' : '' ?>>
-                        <?= $category['category_name'] ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-            <?php if (isset($errors["category_id"])): ?>
-                <p class="error"><?= $errors["category_id"] ?></p>
-            <?php endif; ?>
-        </div>
-
-        <div>
-            <label for="content">Saturs:</label>
-            <textarea id="content" name="content"><?= $_POST['content'] ?? '' ?></textarea>
-            <?php if (isset($errors["content"])): ?>
-                <p class="error"><?= $errors["content"] ?></p>
-            <?php endif; ?>
-        </div>
+        <label for="name">
+            Kategorijas nosaukums:
+            <input type="text" name="name" id="name" value="<?= htmlspecialchars($_POST['name'] ?? '') ?>" required>
+        </label>
 
         <button class="button" type="submit">Izveidot</button>
+
+        <?php if (isset($errors["name"])): ?>
+            <p class="error"><?= htmlspecialchars($errors["name"]) ?></p>
+        <?php endif; ?>
     </form>
 </div>
 
