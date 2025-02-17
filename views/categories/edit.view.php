efcfbd700f1e8ff13c8368f6fb25c26db96529d1
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:28:44 +0200

    Update edit.view.php

--- views/categories/edit.view.php
+++ views/categories/edit.view.php
@@ -1,35 +1,17 @@
 <?php require "views/component/header.php"; ?>
 <?php require "views/component/navbar.php"; ?>
 
-<h1>Rediģēt bloga ierakstu</h1>
+<h1>Rediģēt kategoriju</h1>
 <div class="container">
     <form method="POST">
-        
         <input type="hidden" name="id" value="<?= $category['id'] ?>" />
+        <label for="category_name">Kategorijas nosaukums:</label>
+        <input type="text" id="category_name" name="category_name" value="<?= htmlspecialchars($category['category_name']) ?>" required />
+        <button type="submit">Saglabāt izmaiņas</button>
+    </form>
 
-        <label for="category_id">Kategorija:</label>
-        <select id="category_id" name="category_id">
-        
-            <?php foreach ($categories as $category_option): ?>
-                <option value="<?= $category_option['id'] ?>" 
-                    <?= ($category['id'] == $category_option['id']) ? 'selected' : '' ?>>
-                    <?= $category_option['category_name'] ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-        
-        <?php if (isset($errors["category_id"])): ?>
-            <p class="error"><?= $errors["category_id"] ?></p>
-        <?php endif; ?>
-
-        <label for="content">Saturs:</label>
-        <input type="text" id="content" name="content" value="<?= $_POST['content'] ?? $category['category_name'] ?>" />
-        
-        <?php if (isset($errors["content"])): ?>
-            <p class="error"><?= $errors["content"] ?></p>
-        <?php endif; ?>
-
-        <button class="button" type="submit">Saglabāt izmaiņas</button>
+    <form method="POST" action="/categories/delete">
+        <button type="submit"  >Dzēst kategoriju</button>
     </form>
 </div>
 
