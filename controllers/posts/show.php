97b3462732f93ee4009ebe519184f96634365b62
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:27:26 +0200

    Update show.php

--- controllers/posts/show.php
+++ controllers/posts/show.php
@@ -1,23 +1,23 @@
-<?php
-
+<?php 
 require "Database.php";
 
-if(!isset($_GET["id"]) || $_GET["id"] == ""){
- redirectIFNotFound();
-    
-}
-    $post = $_GET["id"];    
-
-    $sql = "SELECT posts.*, categories.category_name 
-    FROM posts
-    LEFT JOIN categories ON posts.category_id = categories.id
-    WHERE posts.id = :id";
-    $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
-
-if(!$post){
+if (!isset($_GET["id"]) || empty($_GET["id"])) {
     redirectIFNotFound();
 }
-$pageTitle ="Postii";
+
+$sql = "SELECT posts.*, categories.category_name 
+        FROM posts
+        LEFT JOIN categories ON posts.category_id = categories.id
+        WHERE posts.id = :id";
+
+$params = ["id" => $_GET["id"]];
+$post = $db->query($sql, $params)->fetch();
+
+// Ja nav atrasts ieraksts, pārtrauc lapas ielādi
+if (!$post) {
+    redirectIFNotFound();
+}
+
+$pageTitle = "Postii";
 $style = "css/kopejais-stils.css";
-require "views/categories/show.view.php";
+require "views/posts/show.view.php";
