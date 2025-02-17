6e0f7101fa2b42a62b74d9dce4d843d9d152b968
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 10:17:20 +0200

    Update navbar.php

--- views/component/navbar.php
+++ views/component/navbar.php
@@ -5,6 +5,7 @@
             <li><a href="/about">Story </a></li>
             <li><a href="/categories">kategorijas </a></li>
             <li><a href="/create">createe </a></li>
+            <li><a href="categories/create">Izveido kategoriju </a></li>
         </ul>
     </nav>
 </header>
