35ee071bfa9e9010735a6a997f28a4695fea8d69
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 11:07:35 +0200

    Update index.php

--- controllers/categories/index.php
+++ controllers/categories/index.php
@@ -11,14 +11,15 @@ $db = new Database($config["database"]);
 $select = "SELECT * FROM categories"; 
 
 $params = [];
-if (isset($_GET["search_query"])  && $_GET["search_query"] != ""){
+
+if (isset($_GET["search_query"])  && $_GET["categorysearch_query_name"] != ""){
    echo "Atgriest ierakstus";
-   $search_query = "%" . $_GET["search_query"] . "%";
-   $select .= " WHERE content LIKE  :nosaukums"; 
+   $category_name = "%" . $_GET["search_query"] . "%";
+   $select .= " WHERE category_name LIKE  :nosaukums"; 
    $params = ["nosaukums" => $search_query];               
 }
-$posts = $db->query($select, $params)->fetchAll();
+$categories = $db->query($select, $params)->fetchAll();
 
-$pageTitle = "Blogs";
+$pageTitle = "Kategorija";
 $style = "css/kopejais-stils.css";
 require "views/categories/index.view.php";  
