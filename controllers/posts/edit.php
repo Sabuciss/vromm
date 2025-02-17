f881dc450c771b79300bc51637d73798e0e3e64f
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:26:52 +0200

    Update edit.php

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -3,12 +3,17 @@
 require "Database.php";
 require "Validator.php";
 
-if(isset($_GET["id"])){
-    $sql = "SELECT * FROM posts WHERE id = :id;"; 
-    $params = ["id" => $_GET["id"]];
-    $post = $db->query($sql, $params)->fetch();
+if (!isset($_GET["id"]) || empty($_GET["id"])) {
+    redirectIFNotFound();
 }
 
+$sql = "SELECT * FROM posts WHERE id = :id;"; 
+$params = ["id" => $_GET["id"]];
+$post = $db->query($sql, $params)->fetch();
+
+if (!$post) {
+    redirectIFNotFound();
+}
 $sql = "SELECT * FROM categories";
 $categories = $db->query($sql, [])->fetchAll();
 
@@ -22,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     elseif (empty($errors)) {
         $sql = "UPDATE posts SET content = :content, category_id = :category_id WHERE id = :id";
         $params = ["id" => $_POST["id"], "content" => $_POST["content"], "category_id" => $_POST["category_id"] ];
-        $post = $db->query($sql, $params)->fetch();
+        $db->query($sql, $params);
 
         header("Location: /show?id=" . $_POST["id"]);
         exit();
