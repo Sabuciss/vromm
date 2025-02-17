a92899b4544738ac6bcb205402132d7d8c2d4110
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:29:01 +0200

    Update index.view.php

--- views/categories/index.view.php
+++ views/categories/index.view.php
@@ -1,22 +1,17 @@
-<?php require "views/component/header.php";?> <!-- p h p ir jabut lai stradatu-->
-<?php require "views/component/navbar.php";?>
- <h1>Kategorijasss</h1>
+<?php require "views/component/header.php"; ?>
+<?php require "views/component/navbar.php"; ?>
 
-<div class="container">
-   <form >
-      <input name='search_query' value='<?= $_GET["search_query"] ?? ""?>'  />
-      <button>Meklēt</button>
-   </form>
+<h1>Kategorijas</h1>
 
-   <?php if (count($categories) == 0 ){ ?>
-      <p>Not found. its dead 😎</p>
-   <?php } ?>
+<ul>
+    <?php foreach ($categories as $category): ?>
+        <li>
+            <!-- Saites uz rediģēšanu (kategorija tiek nosūtīta ar ID) -->
+            <a href="edit.php?id=<?= urlencode($category['id']) ?>">
+                <?= htmlspecialchars($category["category_name"]) ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
 
-   <ul>
-      <?php foreach($categories as $category){ ?>
-      <li><a href="show?id=<?= $category["id"]?>"> <?= htmlspecialchars($category["content"])?></a></li> 
-      <?php   } ?>
-   </ul>
-</div>
-
-  <?php require "views/component/footer.php";?>
+<?php require "views/component/footer.php"; ?>
