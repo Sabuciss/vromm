953c7770e80920a12c54baef5a20c4478674cd9d
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:43:27 +0200

    Update migrations.sql

--- migrations.sql
+++ migrations.sql
@@ -1,38 +1,34 @@
-/* Izveido jaunu datu bāzi */
 DROP DATABASE IF EXISTS blog_ipb23;
 CREATE DATABASE blog_ipb23;
-
 USE blog_ipb23;
-
-/* Izveido posts tabulu */
-CREATE TABLE posts (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    content VARCHAR(5200),
-    category_id INT NULL
-);
 
 /* Izveido categories tabulu */
 CREATE TABLE categories (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(25)
+    category_name VARCHAR(25) NOT NULL
 );
 
-/* Ievieto datus kategoriju tabulā */
-INSERT INTO categories (category_name)
-VALUES 
-("Sports"),
-("Mūzika"),
+/* Izveido posts tabulu ar ārējo atslēgu */
+CREATE TABLE posts (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content VARCHAR(5200) NOT NULL,
+    category_id INT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
+);
+
+/* Ievieto kategorijas */
+INSERT INTO categories (category_name) VALUES 
+("Sports"), 
+("Mūzika"), 
 ("Svētki");
 
-/* Ievieto datus postu tabulā */
-INSERT INTO posts (content, category_id)
-VALUES 
-("BMW", 1),
-("Kebabs", 2),
+/* Ievieto bloga ierakstus */
+INSERT INTO posts (content, category_id) VALUES 
+("BMW", 1), 
+("Kebabs", 2), 
 ("Santorini", 3);
 
-/* Pārbaudi, kā izskatās saistītie dati */
+
 SELECT posts.*, categories.category_name 
 FROM posts
-LEFT JOIN categories ON posts.category_id = categories.id
-WHERE posts.id = 1;
+LEFT JOIN categories ON posts.category_id = categories.id;
