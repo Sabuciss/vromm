e6e2fccea1ca4d44fc5a6cdd37d79d4622864e30
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:25:08 +0200

    Update edit.php

--- controllers/categories/edit.php
+++ controllers/categories/edit.php
@@ -1,36 +1,39 @@
-
 <?php
 require "Database.php";
-
 require "Validator.php";
 
-$sql = "SELECT * FROM categories"; 
-$categories = $db->query($sql, [])->fetchAll();
+if (isset($_GET['id'])) {
+    $categoryId = $_GET['id'];
 
-if(isset($_GET["id"])){
-    $sql = "SELECT * FROM categories WHERE id = :id;"; 
-    $params = ["id" => $_GET["id"]];
-    $category = $db->query($sql, $params)->fetch();
+    $sql = "SELECT * FROM categories WHERE id = :id";
+    $category = $db->query($sql, ['id' => $categoryId])->fetch();
+
+    if (!$category) {
+        die("Kategorija netika atrasta.");
+    }
+} else {
+    die("Kategorijas ID nav norādīts.");
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $errors = [];
-    
-    if (!Validator::string($_POST["category_name"], max: 50)){
-    $errors["category_name"]= "Saturam ir jabut";
-    }
-   
-    elseif (empty($errors)) {
-        $sql = "UPDATE categories SET category_name = :category_name WHERE id = :id;";
-        $params = ["id" => $_POST["id"], "category_name" => $_POST["category_name"] ];
-        $db->query($sql, $params);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['category_name']) && !empty($_POST['category_name'])) {
+        $categoryName = $_POST['category_name'];
 
-        header("Location: /categories/show?id=" . $_POST["id"]);
-        exit();
+        if (!Validator::string($categoryName, min: 3, max: 50)) {
+            $errors['category_name'] = "Kategorijas nosaukumam jābūt no 3 līdz 50 rakstzīmēm.";
+        } else {
+            $sql = "UPDATE categories SET category_name = :category_name WHERE id = :id";
+            $db->query($sql, ['category_name' => $categoryName, 'id' => $categoryId]);
+
+            header("Location: /categories/show?id=" . $categoryId);
+            exit();
+        }
+    } else {
+        $errors['category_name'] = "Kategorijas nosaukums ir nepieciešams.";
     }
 }
-var_dump($categories);
 
- $pageTitle = "Kategorijas edits";
- $style = "css/kopejais-stils.css";
- require "views/categories/edit.view.php";
+$pageTitle = "Kategorijas rediģēšana";
+$style = "css/kopejais-stils.css";
+require "views/categories/edit.view.php";
+?>
