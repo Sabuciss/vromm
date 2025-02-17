462e964c02bc4967e13412bd9607810d1ab38cc1
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 21:29:31 +0200

    Update show.view.php

--- views/categories/show.view.php
+++ views/categories/show.view.php
@@ -1,14 +1,10 @@
-<?php require "views/component/header.php";?> 
-<?php require "views/component/navbar.php";?>
+<?php require "views/component/header.php"; ?>
+<?php require "views/component/navbar.php"; ?>
 
-<h1><?= htmlspecialchars($post["category_name"])?></h1>
+<h1><?= htmlspecialchars($category["category_name"]) ?></h1>
 
-<button class="button"><a href="edit?id=<?=$post["id"] ?>">Rediģēt </a><button>
- 
- <form method="POST" action="/categories/delete" >
- <input name="id" value="<?= $post["id"]  ?>" type="hidden" />
- <button type="submit"class="button">dzest</button>
-</form> 
+<form method="POST" action="/categories/delete?id=<?= $category['id'] ?>">
+    <button type="submit" >Dzēst kategoriju</button>
+</form>
 
-
-<?php require "views/component/footer.php";?>
+<?php require "views/component/footer.php"; ?>
