8b0dbbc3dcdf0edbfcd9d3ecf4433e1b662ce702
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-17 11:09:05 +0200

    Update show.php

--- controllers/categories/show.php
+++ controllers/categories/show.php
@@ -1,14 +1,15 @@
-<?php require "views/component/header.php";?> 
-<?php require "views/component/navbar.php";?>
+<?php
 
-<h1><?= htmlspecialchars($post["category_name"])?></h1>
+require "Database.php";
 
-<button class="button"><a href="edit?id=<?=$post["id"] ?>">Rediģēt </a><button>
- 
- <form method="POST" action="/categories/delete" >
- <input name="id" value="<?= $post["id"]  ?>" type="hidden" />
- <button type="submit"class="button">dzest</button>
-</form> 
+    $sql = "SELECT * FROM categories WHERE id = :id ;";
+    $params = ["id" => $_GET["id"]];
+    $category = $db->query($sql, $params)->fetch();
 
+if(!$post){
+    redirectIFNotFound();
+}
 
-<?php require "views/component/footer.php";?>
+$pageTitle ="kategorijas";
+$style = "/css/kopejais-stils.css";
+require "views/posts/show.view.php";
