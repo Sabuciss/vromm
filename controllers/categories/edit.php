a581df5a640b4ed2deff6dfa74cd37f5bb1c5f21
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-18 09:47:22 +0200

    Update edit.php

--- controllers/categories/edit.php
+++ controllers/categories/edit.php
@@ -6,31 +6,24 @@ if (isset($_GET['id'])) {
     $categoryId = $_GET['id'];
 
     $sql = "SELECT * FROM categories WHERE id = :id";
-    $category = $db->query($sql, ['id' => $categoryId])->fetch();
+    $category = $db->query($sql, ["id" => $categoryId])->fetch();
 
-    if (!$category) {
-        die("Kategorija netika atrasta.");
     }
-} else {
-    die("Kategorijas ID nav norādīts.");
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['category_name']) && !empty($_POST['category_name'])) {
-        $categoryName = $_POST['category_name'];
+ 
 
-        if (!Validator::string($categoryName, min: 3, max: 50)) {
+        if (!Validator::string($_POST["category_name"], min: 3, max: 50)) {
             $errors['category_name'] = "Kategorijas nosaukumam jābūt no 3 līdz 50 rakstzīmēm.";
         } else {
             $sql = "UPDATE categories SET category_name = :category_name WHERE id = :id";
-            $db->query($sql, ['category_name' => $categoryName, 'id' => $categoryId]);
+           $params =  ["category_name" => $_POST["category_name"], "id" => $_POST["id"]];
+           $category = $db->query($sql, $params)->fetch();
 
-            header("Location: /categories/show?id=" . $categoryId);
+            header("Location: /categories/index");
             exit();
         }
-    } else {
-        $errors['category_name'] = "Kategorijas nosaukums ir nepieciešams.";
-    }
+}
 }
 
 $pageTitle = "Kategorijas rediģēšana";
