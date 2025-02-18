5ddce2fcc547f9124669c651aa9c033295053fad
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-02-18 09:47:04 +0200

    Update index.php

--- controllers/categories/index.php
+++ controllers/categories/index.php
@@ -5,15 +5,7 @@ require "Database.php";
 $config = require("config.php");
 
 $sql = "SELECT * FROM categories"; 
-$categories = $db->query($sql, [])->fetchAll();
-
 $params = [];
-
-if (isset($_GET["search_query"]) && $_GET["search_query"] != "") {
-   $category_name = "%" . $_GET["search_query"] . "%";
-   $sql .= " WHERE category_name LIKE :category_name"; 
-   $params = ["category_name" => $category_name];               
-}
 
 $categories = $db->query($sql, $params)->fetchAll();
 
